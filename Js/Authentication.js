a0d6f42928b4610f8bc0f85d8efd0fdb8b4cf514
OtavioMartins08 <otaviomartinsluis281@outlook.com>
2023-09-08 11:31:13 -0300

    Fix: Register Page

--- Js/Authentication.js
+++ Js/Authentication.js
@@ -2,11 +2,34 @@ const wrapperauthentication = document.querySelector('.wrapper-authentication');
 const createaccountlink = document.querySelector('.create-account-link');
 
 createaccountlink.onclick = (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
 
     if (wrapperauthentication.classList.contains('active')) {
-        wrapperauthentication.classList.remove('active'); 
+        wrapperauthentication.classList.remove('active');
     } else {
-        wrapperauthentication.classList.add('active'); 
+        wrapperauthentication.classList.add('active');
     }
 }
+
+const passwordField = document.getElementById('register-password');
+const infoTextPassword = document.querySelector('.info-text-password');
+
+passwordField.addEventListener('click', () => {
+    infoTextPassword.style.display = 'block';
+});
+
+passwordField.addEventListener('blur', () => {
+    infoTextPassword.style.display = 'none';
+});
+
+const select = document.getElementById('select');
+const fileInput = document.querySelector('.authenttication-input.file');
+
+select.addEventListener('change', function () {
+    if (select.value === 'aluno-graduacao') {
+        fileInput.style.display = 'block';
+    } else {
+        fileInput.style.display = 'none';
+    }
+});
+
