b8596e3d406957e29793a75ee71ff1fc9a6c2fa0
ZirlangioFilho <116697703+ZirlangioFilho@users.noreply.github.com>
2023-09-11 13:18:31 -0300

    Fix: efeito do carrossel

--- Pages/js/courses.js
+++ Pages/js/courses.js
@@ -1,15 +1,32 @@
 
 const swiper = new Swiper('.swiper', {
-    cssMode: true,
-    loop: true,
     freeMode:true,
+    loop: true,
+    loopPreventsSlide: false,
+    keyboard:true,
+    mousewheel: true,
     slidesPerView: 2.5,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
   });
-
+  
   window.addEventListener('resize', function() {
     // Verifique o tamanho da tela usando uma media query
-    if (window.innerWidth <= 650 && window.innerWidth > 490) { // Por exemplo, quando a largura da tela é igual ou menor que 768px
-        swiper.params.slidesPerView = 1.5; // Atualize slidesPerView para 1.5
+    if (window.innerWidth <= 650 && window.innerWidth > 490) { 
+
+        swiper.params.effect = 'coverflow', // Aplica o efeito "coverflow"
+        swiper.params.coverflowEffect = {
+        rotate: 50, // ângulo de rotação
+        stretch: 100, // esticamento das imagens
+        depth: 100, // profundidade do efeito
+        modifier: 0, // multiplicador do efeito
+        slideShadows: false,
+        
+        
+    };
+      
     } else if (window.innerWidth <= 490) {
         swiper.params.slidesPerView = 1
         }
@@ -19,3 +36,57 @@ const swiper = new Swiper('.swiper', {
 
     swiper.update(); // Atualize o Swiper para refletir as alterações
 });
+window.addEventListener('resize', function() {
+if (window.innerWidth <= 900 && window.innerWidth > 490) {
+    slidesPerView = 1.5,
+    swiper.params.effect = 'coverflow';
+    swiper.params.coverflowEffect = {
+        rotate: 50,
+        stretch: 100,
+        depth: 100,
+        modifier: 0,
+        slideShadows: true,
+    };
+    
+   
+} else if (window.innerWidth <= 490) {
+    swiper.params.slidesPerView = 1;
+} else {
+    swiper.params.slidesPerView = 2.5;
+}
+
+})
+
+// var swiper = new Swiper('.swiper', {
+//     freeMode: true,
+//     loop: true,
+//     loopPreventsSlide: false,
+//     keyboard: true,
+//     mousewheel: true,
+//     slidesPerView: 2.5,
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+// });
+
+// function updateSwiper() {
+//     if (window.innerWidth <= 900) {
+//         swiper.params.effect = 'coverflow';
+//         swiper.params.coverflowEffect = {
+//             rotate: 50,
+//             stretch: 100,
+//             depth: 100,
+//             modifier: 0,
+//             slideShadows: true,
+//         };
+//     } else {
+//         swiper.params.effect = 'slide';
+//         swiper.params.coverflowEffect = null;
+//     }
+
+//     swiper.update();
+// }
+
+// window.addEventListener('resize', updateSwiper);
+// updateSwiper();
