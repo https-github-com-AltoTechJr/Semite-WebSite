9cf8b7d123348780e0e56745dcc6740a51eb4411
Wallacy fernandez <111294600+WallacyFernandez@users.noreply.github.com>
2023-09-18 15:23:37 -0300

    Add: new events

--- Pages/js/courses.js
+++ Pages/js/courses.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        speed: 2000, 
+        speed: 1500, 
         loopAdditionalSlides: 1, 
         loopPreventsSlide: false, 
         autoplay: {
@@ -16,24 +16,16 @@ document.addEventListener('DOMContentLoaded', function () {
             disableOnInteraction: false,
         },
         disableOnClick: false,
-    });
+        breakpoints: {
+            0: {
+                slidesPerView: 1.7,
+                
+            },
 
-    window.addEventListener('resize', function () {
-        if (window.innerWidth <= 835 && window.innerWidth > 490) {
-            swiper.params.slidesPerView = 1.5,
-            swiper.params.effect = 'coverflow',
-            swiper.params.coverflowEffect = {
-                rotate: 50,
-                stretch: 100,
-                depth: 100,
-                modifier: 1,
-                slideShadows: false,
-            };
-        } else if (window.innerWidth <= 490) {
-            swiper.params.slidesPerView = 1.5
-        } else {
-            swiper.params.slidesPerView = 2.5;
-        }
-        swiper.update(); // Atualiza o carrossel após a mudança de configuração
+            700: {
+                slidesPerView: 2.5,
+                
+            },
+        },
     });
 });
